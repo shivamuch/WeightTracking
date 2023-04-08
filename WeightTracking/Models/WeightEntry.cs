3277204172a349d78f19cb7058faa602c8ef6da0
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-08 04:11:36 -0400

    Added API classes in library

--- WeightTracking/Models/WeightEntry.cs
+++ WeightTracking/Models/WeightEntry.cs
@@ -1,11 +1,10 @@
-﻿using System;
-namespace WeightTracking.Models
+﻿namespace WeightTracking.Models
 {
-    public class WeightEntry
-    {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public decimal Weight { get; set; }
-        public string UserId { get; set; }
-    }
+	public class WeightEntry
+	{
+		public WeightEntry()
+		{
+		}
+	}
 }
+
---
+++ WeightTrackingLibrary/ApiClient/WeightApiClient.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WeightTrackingLibrary.ApiClient;
+
+namespace WeightTrackingLibrary.ApiClient
+{
+    public class WeightApiClient<T>
+    {
+        private readonly HttpClient client;
+
+        public WeightApiClient()
+        {
+            client = new();
+
+        }
+
+        public WeightApiClient(IConfiguration configuration) : this()
+        {
+        }
+
+        public async Task<List<T>> GetWeight(IEnumerable<string?> API_KEY)
+        {
+            string WEIGHT_API_URL = null;
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(WEIGHT_API_URL),
+                Headers =
+            {
+                { "X-RapidAPI-Key", API_KEY },
+                { "X-RapidAPI-Host", "flixster.p.rapidapi.com" },
+            },
+            };
+            using var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var body = await response.Content.ReadAsStringAsync();
+            return CreateWeightsFromJson(body);
+        }
+
+        public List<T> CreateWeightsFromJson(string json)
+        {
+            var res = JsonConvert.DeserializeObject<Response<T>>(json);
+            return res?.data?.upcoming!;
+        }
+    }
+}
