14675bab9d138472ed4440550ded8f25465c91dc
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-04-14 18:55:21 -0400

    added controllers to the main weight tracking app project and
    added properties, json file and new page support

---
+++ WeightTrackingApp/Controllers/BMICalculaterController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WeightTrackingApp.Controllers
+{
+	public class BMICalculaterController
+	{
+		public BMICalculaterController()
+		{
+		}
+	}
+}
+
---
+++ WeightTrackingApp/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WeightTrackingApp.Controllers
+{
+	public class HomeController
+	{
+		public HomeController()
+		{
+		}
+	}
+}
+
